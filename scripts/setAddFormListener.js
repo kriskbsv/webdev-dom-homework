552d7673c22f48d33fb39d1b1ac53740fb6f61b0
Kris <kabisovak@icloud.com>
2026-09-07 12:07:02 +0300

    Refactor setAddFormListener for better async handling
    Refactor add form listener to use promises for handling comments and loading states.

--- scripts/setAddFormListener.js
+++ scripts/setAddFormListener.js
@@ -1,17 +1,27 @@
-import { renderComments } from "./renderComments.js";
 import { loadComments, postComment } from "./api.js";
+import { renderComments } from "./renderComments.js";
 
 const nameInput = document.querySelector(".add-form-name");
 const textInput = document.querySelector(".add-form-text");
 const addButton = document.querySelector(".add-form-button");
+const addForm = document.querySelector(".add-form");
+const addFormLoader = document.querySelector(".add-form-loader");
 
 export function setAddFormListener() {
-  addButton.addEventListener("click", async () => {
-    await postComment({ name: nameInput.value, text: textInput.value });
-    await loadComments();
-    renderComments();
+  addButton.addEventListener("click", () => {
+    addForm.style.display = "none";
+    addFormLoader.style.display = "block";
 
-    nameInput.value = "";
-    textInput.value = "";
+    postComment({ name: nameInput.value, text: textInput.value })
+      .then(() => loadComments())
+      .then(() => {
+        renderComments();
+        nameInput.value = "";
+        textInput.value = "";
+      })
+      .finally(() => {
+        addForm.style.display = "";
+        addFormLoader.style.display = "none";
+      });
   });
 }
