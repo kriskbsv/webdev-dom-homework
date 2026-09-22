003c6917e293d3c7023e0ebf29836c9abce39f9d
Kris <kabisovak@icloud.com>
2026-09-23 00:10:46 +0300

    Move textInput declaration inside setCommentListeners

--- scripts/setCommentListeners.js
+++ scripts/setCommentListeners.js
@@ -1,15 +1,15 @@
 import { comments } from "./comments.js";
 
-const textInput = document.querySelector(".add-form-text");
-
 export function setCommentListeners() {
   const commentElements = document.querySelectorAll(".comment");
-
   commentElements.forEach((commentElement) => {
     commentElement.addEventListener("click", () => {
+      const textInput = document.querySelector(".add-form-text");
+      if (!textInput) {
+        return; 
+      }
       const index = Number(commentElement.dataset.index);
       const comment = comments[index];
-
       textInput.value = `> ${comment.text}\n${comment.name}, `;
       textInput.focus();
     });
